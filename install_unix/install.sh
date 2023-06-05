9d5434da946362e498c697c9e65623d9beaaa636
grdn1337 <dev@grdn.rocks>
2023-06-05 08:40:59 +0200

    Add .env generator to install, move service file

--- install_unix/install.sh
+++ install_unix/install.sh
@@ -5,7 +5,7 @@ systemctl disable pihole-mqtt.service
 
 echo "Copy files"
 
-cp ./service/pihole-mqtt.service /lib/systemd/system/
+cp ./pihole-mqtt.service /lib/systemd/system/
 
 cp ../async.py /usr/bin/pihole-MQTT
 chmod +x /usr/bin/pihole-MQTT
@@ -13,6 +13,19 @@ chmod +x /usr/bin/pihole-MQTT
 echo "/etc directory check"
 
 [ -d /etc/pihole-MQTT ] || mkdir /etc/pihole-MQTT
+
+if [ ! -f /etc/pihole-MQTT/.env ]
+then
+    touch /etc/pihole-MQTT/.env
+
+    echo "MQTT_HOST = 'your host ip or dns name'" >> /etc/pihole-MQTT/.env
+    echo "MQTT_PORT = 1883" >> /etc/pihole-MQTT/.env
+    echo "MQTT_USER = 'your mqtt username'" >> /etc/pihole-MQTT/.env
+    echo "MQTT_PASS = 'your mqtt password'" >> /etc/pihole-MQTT/.env
+    echo "MQTT_TOPIC_WILL = 'mikrotik/pihole-mqtt'" >> /etc/pihole-MQTT/.env
+    echo "MQTT_TOPIC_DNSCONFIG = 'mikrotik/dns-config'" >> /etc/pihole-MQTT/.env
+    echo "MQTT_RECONNECT_INTERVAL = 5" >> /etc/pihole-MQTT/.env
+fi
 
 echo "Daemon reload"
 
